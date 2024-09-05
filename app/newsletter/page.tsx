aa01ca81bf12dafde8a74cd582d37aa0a6948b19
Kevin <kevin@gep.hu>
2024-09-05 16:01:53 +0200

    Newsletter and unsubscribe pages are done.

--- app/newsletter/page.tsx
+++ app/newsletter/page.tsx
@@ -1,8 +1,46 @@
-import React from 'react'
+'use client';
+import { SyntheticEvent, useState, KeyboardEvent } from "react";
+import Link from "next/link";
 
 const Page = () => {
+
+  const [checkBoxValue, setCheckboxValue] = useState<boolean>(false);
+
+  const handleKeyboardCheckbox = (e: KeyboardEvent<SVGRectElement>) => {
+    if(e.code === 'Space' || e.code=== 'Enter') setCheckboxValue(!checkBoxValue);
+  }
+
+  const handleSubmit = (e: SyntheticEvent) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>Page</div>
+    <div className="min-h-[90vh] lg:ml-80 w-[500px]">
+      <h2 className="mt-20 text-3xl mb-16 font-bold">Subscribe for the newsletters</h2>
+      <p className="mb-10">If you would like to be informed the valuable news, subscribe for our newsletters.</p>
+      <form action="#" onSubmit={handleSubmit}>
+        <input type="email" name="email" placeholder="Email" required className="block w-[100%] mb-3 border border-gray-400 dark:border-current p-2 pt-1 pb-1 rounded-sm focus-within:outline-none"/>
+        <input type="text" name="name" placeholder="Name" required className="block w-[100%] mb-4 border border-gray-400 dark:border-current p-2 pb-1 pt-1 rounded-sm focus-within:outline-none"/>
+        
+        <div className="checkbox-wrapper-62 mb-4" >
+            <input type="checkbox" className="check" id="check1-63" checked={checkBoxValue} required/>
+            <label htmlFor="check1-63" className="label1 flex gap-2 items-center cursor-pointer" onClick={() => setCheckboxValue(!checkBoxValue)}>
+              <svg width="30" height="30" viewBox="0 0 90 90">
+                <rect x="30" y="20" width="50" height="50" stroke="currentColor" fill="none" className='focus:outline  outline-base-content' tabIndex={0} onKeyDown={handleKeyboardCheckbox} />
+                <g transform="translate(0,-952.36218)">
+                  <path d="m 13,983 c 33,6 40,26 55,48 " stroke="currentColor" stroke-width="3" className="path1" fill="none" />
+                  <path d="M 75,970 C 51,981 34,1014 25,1031 " stroke="currentColor" stroke-width="3" className="path1" fill="none" />
+                </g>
+              </svg>
+              <span>By subscribing for newsletters, you agree to privacy notice.</span>
+            </label>
+          </div>
+        <input type="submit" value="Subscribe" className="border p-2 cursor-pointer border-gray-400 dark:border-current font-semibold bg-gray-100 hover:bg-gray-50 dark:bg-gray-600 dark:hover:bg-gray-500 rounded-sm"/>
+      </form>
+      <div>
+        <div className="mt-10 text-xs">Would not you like to get letters? <Link href='/newsletter/unsubscribe' className='text-slate-400'>Unsubscribe</Link></div> 
+      </div>
+    </div>
   )
 }
 
---
+++ app/newsletter/unsubscribe/page.tsx
@@ -0,0 +1,24 @@
+'use client';
+import { SyntheticEvent } from 'react'
+
+const Page = () => {
+  const handleSubmit = (e: SyntheticEvent) => {
+    e.preventDefault();
+  }
+
+  return (
+    <div className='min-h-[90vh] mt-20 lg:ml-80 w-[500px]'>
+      <p className='mb-2'>If you would like to unsubscribe, enter your email.</p>
+      <form action="#" className='relative' onSubmit={handleSubmit}>
+        <input type="email" name="email" className='block w-[100%] border border-gray-400 dark:border-current p-2 pt-1 pb-1 rounded-sm focus-within:outline-none'/>
+        <button className='absolute right-1 top-1'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+          </svg>
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default Page
