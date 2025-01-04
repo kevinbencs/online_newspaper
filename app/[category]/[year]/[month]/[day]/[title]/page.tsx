bf40b9c9a666481cf0985ba30ff16f15b4695eb5
Kevin <kevinbencs8@gmail.com>
2025-01-05 00:04:04 +0100

    Change x card for summary.

--- app/[category]/[year]/[month]/[day]/[title]/page.tsx
+++ app/[category]/[year]/[month]/[day]/[title]/page.tsx
@@ -42,7 +42,7 @@ export async function generateMetadata({ params, searchParams }: { params: { cat
     twitter:{
       title: params.title.replaceAll('_', ' '),
       description: res.data?.description,
-      card: 'app',
+      card: 'summary',
       images: [
         {
           url: res.data ? res.data?.cover_img_id.split(';')[0] : '',
