9d3746ea39172e3933807a82bbbf50a3d4cf3698
Kevin <kevinbencs8@gmail.com>
2025-05-11 12:56:45 +0200

    ixed sidebar error (My artcile)

--- app/_components/sidebar/userelement.tsx
+++ app/_components/sidebar/userelement.tsx
@@ -104,7 +104,7 @@ const UserElement = (props: { setCheckboxValue: Dispatcher<boolean> }) => {
 
                 New article
             </Link></li>
-            <li><Link onClick={checked} href={`/authors/${WhoLogged.replaceAll(' ', '_')}`} className='pt-1 pb-1 bg-neutral text-neutral-content mt-2 ml-2 mr-2 md:text-xl text-base  rounded-[4px] hover:bg-slate-100 hover:text-black flex justify-center gap-2'>
+            <li><Link onClick={checked} href={`/authors/${WhoLogged.replaceAll(' ', '_')}/1`} className='pt-1 pb-1 bg-neutral text-neutral-content mt-2 ml-2 mr-2 md:text-xl text-base  rounded-[4px] hover:bg-slate-100 hover:text-black flex justify-center gap-2'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z" />
                 </svg>
